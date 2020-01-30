c1a91fba6005ef0976beefffa565dd3837233391
Ivan <ivangpajon@hotmail.com>
2020-01-30 21:21:00 +0100

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatDividerModule } from '@angular/material/divider';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatChipsModule } from '@angular/material/chips';
+import { TooltipComponent } from './components/tooltip/tooltip.component';
+import { CustomTooltipDirective } from './directives/custom-tooltip.directive';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TooltipComponent,
+    CustomTooltipDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatChipsModule,
+    OverlayModule,
+    MatDividerModule,
   ],
+  entryComponents: [TooltipComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/components/tooltip/tooltip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tooltip-component',
+  templateUrl: './tooltip.component.html',
+  styleUrls: ['./tooltip.component.scss']
+})
+export class TooltipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
